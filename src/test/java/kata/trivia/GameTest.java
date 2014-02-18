d815b9239fb4f7cc4ec5936b099ade3dfb536997
Ben Wu <wubin28@gmail.com>
2014-02-19 00:46:51 +0800

    Added characterization test "GIVEN_one_player_WHEN_she_answered_correctly_THEN_game_will_continue".

--- src/test/java/kata/trivia/GameTest.java
+++ src/test/java/kata/trivia/GameTest.java
@@ -8,7 +8,14 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
 
     @Test
-    public void testApp() {
-        assertEquals(5, 2 + 3);
+    public void GIVEN_one_player_WHEN_she_answered_correctly_THEN_game_will_continue() {
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+
+        aGame.roll(1);
+        boolean willContinue = aGame.wasCorrectlyAnswered();
+
+        assertEquals(true, willContinue);
     }
 }
