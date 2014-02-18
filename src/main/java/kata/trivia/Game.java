f18cac2824b97a45bc060aec7014977d9924182a
Ben Wu <wubin28@gmail.com>
2014-02-19 00:14:36 +0800

    Fixed the typo "corrent".

--- src/main/java/kata/trivia/Game.java
+++ src/main/java/kata/trivia/Game.java
@@ -116,7 +116,7 @@ public class Game {
 
         } else {
 
-            System.out.println("Answer was corrent!!!!");
+            System.out.println("Answer was correct!!!!");
             purses[currentPlayer]++;
             System.out.println(players.get(currentPlayer)
                     + " now has "
