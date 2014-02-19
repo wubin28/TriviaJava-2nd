bf4109798af8465f49a353a2d244d37136b5cac7
Ben Wu <wubin28@gmail.com>
2014-02-19 20:10:13 +0800

    Created the constructor of Player with the name of the player as the parameter.

--- src/main/java/kata/trivia/server/Player.java
+++ src/main/java/kata/trivia/server/Player.java
@@ -4,7 +4,12 @@ package kata.trivia.server;
  * Created by ben on 14-2-19.
  */
 public class Player {
+    private final String name;
     private int place;
+
+    public Player(String playerName) {
+        this.name = playerName;
+    }
 
     public int getPlace() {
         return place;
