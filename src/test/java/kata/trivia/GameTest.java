ac493d4b07aeafbcf8651b21bd49d7bffcbd765a
Ben Wu <wubin28@gmail.com>
2014-02-19 10:19:27 +0800

    Added test "GIVEN_one_player_WHEN_she_answered_wrongly_for_1_time_but_correctly_for_6_times_THEN_game_will_not_continue".

--- src/test/java/kata/trivia/GameTest.java
+++ src/test/java/kata/trivia/GameTest.java
@@ -49,4 +49,39 @@ public class GameTest {
         willContinue = aGame.wasCorrectlyAnswered();
         assertEquals(false, willContinue);
     }
+
+    @Test
+    public void GIVEN_one_player_WHEN_she_answered_wrongly_for_1_time_but_correctly_for_6_times_THEN_game_will_not_continue() {
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+
+        aGame.roll(1);
+        boolean willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wrongAnswer();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(true, willContinue);
+
+        aGame.roll(1);
+        willContinue = aGame.wasCorrectlyAnswered();
+        assertEquals(false, willContinue);
+    }
 }
